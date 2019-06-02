e75358d71ffd2be32272aa34cb2fc6ec0a3b56a4
Marek <icluelesscoder@gmail.com>
2019-06-02 21:07:48 +0100

    Implemented completeDay function - returning to default once selected is not working.

--- Habit Loop/Controller/ViewController.swift
+++ Habit Loop/Controller/ViewController.swift
@@ -27,9 +27,19 @@ class ViewController: UIViewController {
     @IBAction func dayCompleted(_ sender: UIButton) {
         streakCount += 1
         if let dayNumber = dayButtons.firstIndex(of: sender) {
-            
+            completeDay(dayOfTheMonth: arrayOfDays[dayNumber], on: sender)
+        } else {
+            print("chosen day was not in dayButtons")
         }
         
+    }
+    
+    func completeDay(dayOfTheMonth dayNumber: String, on button: UIButton) {
+        if button.currentTitle == dayNumber {
+            button.backgroundColor = #colorLiteral(red: 0.6189721823, green: 0.6221218109, blue: 1, alpha: 1)
+        } else {
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
+        }
     }
     
 //    UI Labels -----------------------------------------
