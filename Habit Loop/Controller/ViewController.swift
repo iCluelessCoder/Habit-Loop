447853f5fd106ed3fab243b899c22753360fc78e
Marek <icluelesscoder@gmail.com>
2019-06-05 21:09:11 +0100

    Days deleted, going to implement different tracking method.

--- Habit Loop/Controller/ViewController.swift
+++ Habit Loop/Controller/ViewController.swift
@@ -17,44 +17,14 @@ class ViewController: UIViewController {
         }
     }
     
-    var dayCompletStatus = false
-    
-    @IBOutlet var dayButtons: [UIButton]!
-    
-    var arrayOfDays = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th"]
-    
-    
-//    Days Buttons -----------------------------------------
-
-    @IBAction func dayCompleted(_ sender: UIButton) {
-        
-        streakCount += 1
-
-        if let dayNumber = dayButtons.firstIndex(of: sender) {
-            completeDay(dayOfTheMonth: arrayOfDays[dayNumber], on: sender)
-            print("Index of the day selected is: \(dayNumber)")
-            
-        } else {
-            print("chosen day was not in dayButtons")
-        }
-        
-    }
-    
-    func completeDay(dayOfTheMonth dayNumber: String, on button: UIButton) {
-        if button.currentTitle == dayNumber {
-            button.backgroundColor = #colorLiteral(red: 0.6189721823, green: 0.6221218109, blue: 1, alpha: 1)
-        } else {
-            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
-        }
-    }
-    
 //    UI Labels -----------------------------------------
     
     @IBOutlet weak var streakCounter: UILabel!
     
-    @IBAction func resetStreakButton(_ sender: UIButton) {
-        streakCount = 0
-        streakCounter.text = "No Streak!"
+//    UI Buttons ----------------------------------------
+    
+    @IBAction func dayCompleted(_ sender: UIButton) {
+        streakCount += 1
     }
     
     override func viewDidLoad() {
